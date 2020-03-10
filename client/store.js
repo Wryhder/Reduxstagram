8d72a84a638d1d30b8b5b2c6cbee1ff2c84f067f
Wryhder <jemimahsummers@yahoo.com>
2020-03-10 13:20:32 +0100

    Add hot reloading for redux reducers

--- client/store.js
+++ client/store.js
@@ -12,8 +12,19 @@ const defaultState = {
     comments,
 };
 
+const enhancers = compose(
+    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+);
+
 const store = createStore(rootReducer, defaultState);
 
 export const history = syncHistoryWithStore(browserHistory, store);
+
+if (module.hot) {
+    module.hot.accept('./reducers/', () => {
+        const nextRootReducer = require('./reducers/index').default;
+        store.replaceReducer(nextRootReducer);
+    });
+}
 
 export default store;
