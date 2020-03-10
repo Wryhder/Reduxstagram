6d21e7af21491c0f7f3c488035aa12cd681866f7
Wryhder <jemimahsummers@yahoo.com>
2020-03-10 13:32:47 +0100

    Display single photo component along with comments

---
+++ client/components/Comments.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Comments = React.createClass({
+    renderComment(comment, i) {
+        return (
+            <div className="comment" key={i}>
+                <p>
+                    <strong>{comment.user}</strong>
+                    {comment.text}
+                </p>
+            </div>
+        );
+    },
+    render() {
+        return (
+            <div className="comment">
+                {this.props.postComments.map(this.renderComment)}
+            </div>
+        );
+    }
+});
+
+export default Comments;
